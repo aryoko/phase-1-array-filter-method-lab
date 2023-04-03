a58160b84738998abc3d3b9a28450ef7ab3c8fff
Ary Okoli <aco.umeadi@gmail.com>
2023-04-03 11:43:13 -0400

    array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  let filteredNames = drivers.filter(function(driver) {
+   return (driver == name || driver == name.toLowerCase())
+  })
+  return filteredNames
+}
+
+function fuzzyMatch(drivers, firstTwoChar) {
+  let filteredNames = drivers.filter(function(driver) {
+    return (driver.slice(1, 2) === firstTwoChar.slice(1, 2))
+  })
+  return filteredNames
+}
+
+function matchName(drivers, name) {
+  let filteredNames = drivers.filter(function (driver) {
+    return driver.name === name
+  })
+  return filteredNames
+}
